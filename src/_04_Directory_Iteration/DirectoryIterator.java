4903b993a556262a84c45fd220b43ac038e5f728
J1a4b4 <46267154+J1a4b4@users.noreply.github.com>
2020-09-19 17:26:13 -0700

    DirectoryIterator(IP)
    :smiley:

--- src/_04_Directory_Iteration/DirectoryIterator.java
+++ src/_04_Directory_Iteration/DirectoryIterator.java
@@ -10,18 +10,18 @@ public class DirectoryIterator {
 		 * The following is an example of how to list all of the files in a directory.
 		 * Once the program is running, the directory is chosen using the JFileChooser.
 		 */
-		JFileChooser jfc = new JFileChooser();
-		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		int returnVal = jfc.showOpenDialog(null);
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			File directory = jfc.getSelectedFile();
-			File[] files = directory.listFiles();
-			if(files != null) {
-				for(File f : files) {
-				  System.out.println(f.getAbsolutePath());
-				}
-			}
-		}
+		//JFileChooser jfc = new JFileChooser();
+		//jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		//int returnVal = jfc.showOpenDialog(null);
+		//if (returnVal == JFileChooser.APPROVE_OPTION) {
+		//	File directory = jfc.getSelectedFile();
+		//	File[] files = directory.listFiles();
+		//	if(files != null) {
+		//		for(File f : files) {
+		//		  System.out.println(f.getAbsolutePath());
+		//		}
+		//	}
+		//}
 		
 		/*
 		 * Your task is to write a program that iterates through the src folder of this current Java Project. 
@@ -29,5 +29,12 @@ public class DirectoryIterator {
 		 * Be aware of possible directories inside of directories.
 		 * (e.g //Copyright © 2019 FirstName LastName)
 		 */
+		
+	}
+	
+	void iterator() {
+		JFileChooser jfc = new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		
 	}
 }
