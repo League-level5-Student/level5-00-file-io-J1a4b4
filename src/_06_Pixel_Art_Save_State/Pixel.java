94194a61544333c8c5a0b1aca571df24df4fe9d2
J1a4b4 <46267154+J1a4b4@users.noreply.github.com>
2020-10-04 12:57:53 -0700

    Serialization, PixelArtSaveState(IP)
    :smile:

---
+++ src/_06_Pixel_Art_Save_State/Pixel.java
@@ -0,0 +1,17 @@
+package _06_Pixel_Art_Save_State;
+
+import java.awt.Color;
+
+public class Pixel{
+	public int x;
+	public int y;
+	public Color color;
+	
+	public Pixel(int x, int y) {
+		this.x = x;
+		this.y = y;
+		color = Color.WHITE;
+	}
+}
+
+ //Copyright © 2020 League of Amazing Programmers and Jonas Brown
